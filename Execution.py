9ecf5f0665dd0b78afddadd8c768715bea360ea1
Elyes Mahjoubi <65829912+EM51641@users.noreply.github.com>
2020-07-24 16:42:58 +0100

    Update Execution.py

--- Execution.py
+++ Execution.py
@@ -7,11 +7,12 @@ from scipy.optimize import minimize
 from numba import jit
 import statsmodels.api as sm
 
-T     = 4
+T     = 200
 dt    = 1/252
 n=int(T/dt)
-rho=-0.90
-ST,Output,Cond_Var=Stock_Selector('LMT').selector #select your stock
+rho=-0.50
+r=0.05
+ST,Output,Cond_Var=Stock_Selector('TLT').selector
 v0=Cond_Var[-1]
 MU  = np.array([0, 0])
 COV = np.matrix([[1, rho], [rho, 1]])
@@ -20,13 +21,14 @@ W_S = W[:,0]
 W_v = W[:,1]
 
 kappa,theta,xi=Initialize_parameters().kappa_sigma_theta_initial_estimators(dt,Cond_Var)
+print(kappa,theta,xi)
 kappa_list=[]
 theta_list=[]
 xi_list=[]
 p_list=[]
 
-for i in range(1000000):#generate 1 Million Monte carlos simulations
-    H=Initialize_parameters().HeMC (v0, kappa, theta, xi, n, dt,W_v,W_S)
+for i in range(1000000):
+    H=Initialize_parameters().HeMC (r,v0, kappa, theta, xi, n, dt,W_v,W_S)
     kappa_list.append(H[0])
     theta_list.append(H[1])
     xi_list.append(H[2])
@@ -36,4 +38,4 @@ rho   = pd.DataFrame(p_list).dropna().mean()[0] # Correlation
 kappa = pd.DataFrame(kappa_list).dropna().mean()[0]# Revert rate
 theta = pd.DataFrame(theta_list).dropna().mean()[0] # Long-term Variance
 xi    = pd.DataFrame(xi_list).dropna().mean()[0] # Volatility of instantaneous volatility
-print(kappa,theta,xi,rho)#print the parameters
+print(kappa,theta,xi,rho)
