7753b25d3221ab324ed5068691c052a6e3742311
Elyes Mahjoubi <65829912+EM51641@users.noreply.github.com>
2020-12-20 14:30:40 +0100

    Update Execution.py

--- Execution.py
+++ Execution.py
@@ -2,14 +2,13 @@ import numpy as np
 import pandas as pd
 import quandl
 
-dt    = 1/52
+dt    = 1/252
 rfree = quandl.get("USTREASURY/YIELD")['1 YR']
-rfree = rfree.loc['1995-01-01':].resample('1W').last()[1:]/100
 rfree = pd.DataFrame(rfree)
 Kappa,theta,xi = Initialize_parameters().kappa_sigma_theta_initial_estimators(dt,rfree)
-args = [dt,len(Yield),Yield]
+args = [dt,len(rfree),rfree]
 res = minimize(LogL,[Kappa,theta,xi],args,method='SLSQP')
 Kappa,theta,xi=res.x
-week_forecast = 52
-J = 1000
-H = Initialize_parameters().MCR(Yield.iloc[-1,0], Kappa, theta, xi,dt,week_forecast,J)
+day_forecasts = 252
+number_of_try = 1000
+H = Initialize_parameters().MCR(rfree.iloc[-1,0], Kappa, theta, xi,dt,day_forecasts,number_of_try)
