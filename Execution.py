099d03b8b577884e002898775927893e4d7c0995
Elyes Mahjoubi <65829912+EM51641@users.noreply.github.com>
2023-06-26 23:37:59 +0100

    Update Execution.py

--- Execution.py
+++ Execution.py
@@ -1,15 +1,15 @@
 import numpy as np
 import pandas as pd
 import quandl
-import Initialize_parameters
+import CIRModel
 
 dt    = 1/252
 rfree = quandl.get("USTREASURY/YIELD")['1 YR']
 rfree = pd.DataFrame(rfree)
-Kappa,theta,xi = Initialize_parameters().kappa_sigma_theta_initial_estimators(dt,rfree)
+Kappa,theta,xi = CIRModel().kappa_sigma_theta_initial_estimators(dt,rfree)
 args = [dt,len(rfree),rfree]
-res = minimize(Initialize_parameters().LogL,[Kappa,theta,xi],args,method='SLSQP')
+res = minimize(CIRModel().LogL,[Kappa,theta,xi],args,method='SLSQP')
 Kappa,theta,xi=res.x
 day_forecasts = 252
 number_of_trials = 1000
-H = Initialize_parameters().MCR(rfree.iloc[-1,0], Kappa, theta, xi,dt,day_forecasts,number_of_trials)
+H = CIRModel().MCR(rfree.iloc[-1,0], Kappa, theta, xi,dt,day_forecasts,number_of_trials)
